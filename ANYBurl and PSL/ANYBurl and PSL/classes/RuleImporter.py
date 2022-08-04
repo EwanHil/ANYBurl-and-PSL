13ee659c711c1bb6a8bb04472a41cee21b35d84b
EwanHil <110049541+EwanHil@users.noreply.github.com>
2022-08-04 10:26:18 +0100

    Delete ANYBurl and PSL/ANYBurl and PSL directory

--- ANYBurl and PSL/ANYBurl and PSL/classes/RuleImporter.py
+++
@@ -1,9 +0,0 @@
-class RuleImporter():
-    def __init__(self, filename='rules/alpha-10'):
-        self.filename = filename
-        self.rules =[]
-        
-    def import_rules(self):
-        with open(self.filename) as rules:
-            for rule in rules:
-                self.rules.append(rule)   
