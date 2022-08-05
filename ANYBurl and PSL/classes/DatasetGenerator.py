236b4d1c9c54b281a2001b8ae2c63fea5841cbe9
EwanHil <110049541+EwanHil@users.noreply.github.com>
2022-08-05 09:53:50 +0100

    Add files via upload
    Fixed some bugs - now fully creates the datasets required for passing to ANYBurl, whilst removing spaces and commas to avoid issues with reading the entities and relations back in later.

--- ANYBurl and PSL/classes/DatasetGenerator.py
+++ ANYBurl and PSL/classes/DatasetGenerator.py
@@ -4,13 +4,13 @@ class DatasetGenerator():
     def ___init___(self):
         pass
             
-    def generate_dataset_file(self, filename, dataset_name, rels):
-        f = open(f"datasets/data/{dataset_name}/{filename}", "w")
-        entity_converter = EntityConverter()
+    def generate_dataset_file(self, filename, dataset_name, rels, dataset):
+        f = open(f"datasets/data/{dataset_name}/{filename}", "w", encoding="utf-8")
+        entity_converter = EntityConverter(dataset)
         for head,rel,tail in rels:
-          head_name = encode_text(entity_converter.entityindex_to_name[head])
-          rel_name = encode_text(entity_converter.relindex_to_name[rel])
-          tail_name = encode_text(entity_converter.entityindex_to_name[tail])
+          head_name = self.encode_text(entity_converter.entityindex_to_name[head])
+          rel_name = self.encode_text(entity_converter.relindex_to_name[rel])
+          tail_name = self.encode_text(entity_converter.entityindex_to_name[tail])
           f.write(f"{head_name}\t{rel_name}\t{tail_name}\n")
           
         f.close()
--- ANYBurl and PSL/classes/EntityConverter.py
+++ ANYBurl and PSL/classes/EntityConverter.py
@@ -1,17 +1,17 @@
 import json
 
 class EntityConverter():
-    def ___init___():
-        with open('json/codex_entities.json') as f:
+    def __init__(self,dataset):
+        with open('classes/json/codex_entities.json', encoding="utf-8") as f:
           codex_entities = json.load(f)
-        with open('json/codex_relations.json') as f:
+        with open('classes/json/codex_relations.json', encoding="utf-8") as f:
           codex_relations = json.load(f)
         
         self.entityid_to_name = { entityid:codex_entities[entityid]['label'] for entityid in codex_entities }
         self.relid_to_name = { relid:codex_relations[relid]['label'] for relid in codex_relations }
         
-        self.entityindex_to_name = { entityindex:entityid_to_name[entityid] for entityid,entityindex in dataset.entity_to_id.items() }
-        self.relindex_to_name = {relindex:relid_to_name[relid] for relid,relindex in dataset.relation_to_id.items() }
-        self.entityname_to_index = { name:index for index,name in entityindex_to_name.items() }
-        self.relname_to_index = { name:index for index,name in relindex_to_name.items() }
+        self.entityindex_to_name = { entityindex:self.entityid_to_name[entityid] for entityid,entityindex in dataset.entity_to_id.items() }
+        self.relindex_to_name = {relindex:self.relid_to_name[relid] for relid,relindex in dataset.relation_to_id.items() }
+        self.entityname_to_index = { name:index for index,name in self.entityindex_to_name.items() }
+        self.relname_to_index = { name:index for index,name in self.relindex_to_name.items() }
             
