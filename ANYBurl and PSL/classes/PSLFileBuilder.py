1bdb953f2b646603d5c9a468e062995626ef8619
EwanHil <110049541+EwanHil@users.noreply.github.com>
2022-08-05 10:24:59 +0100

    Add files via upload
    Now generates _map.txt files required for PSL from a list of relationships and entities

--- ANYBurl and PSL/classes/PSLFileBuilder.py
+++ ANYBurl and PSL/classes/PSLFileBuilder.py
@@ -1,23 +1,24 @@
 from classes.EntityConverter import EntityConverter
+from classes.DatasetGenerator import DatasetGenerator
 
 class PSLFileBuilder():
     def __init__(self, rels = None):
         if(rels is None):
-            self.Rels = []
+            self.rels = []
         else:
-            self.Rels = rels
+            self.rels = rels
 
-    def build_map_files(self, rels, seperator = "%20"):
-      entity_converter = EntityConverter()
-      for rel in self.Rels:
-        file_name = entity_converter.relindex_to_name[rel].replace(" ", seperator)
-        f = open(f"{file_name}_map.txt", "a")
+    def build_map_files(self, entity_converter, seperator = "%20"): 
+      generator = DatasetGenerator()
+      for relindex,name in entity_converter.relindex_to_name.items():
+         
+        f = open(f"data/{generator.encode_text(name)}_map.txt", "a",  encoding="utf-8")
         first_line = True
         items_added = []
-        for h,r,t in  rels:
-          if r == rel and t not in items_added: #Avoid adding duplicates to the map
+        for h,r,t in  self.rels:
+          if r == relindex and t not in items_added: #Avoid adding duplicates to the map
             items_added.append(t)
-            t_map_name = entity_converter.entityindex_to_name[t]
+            t_map_name = generator.encode_text(entity_converter.entityindex_to_name[t])
             if first_line:
               f.write(f"{t}\t{t_map_name}")
               first_line = False
@@ -25,9 +26,9 @@ class PSLFileBuilder():
               f.write(f"\n{t}\t{t_map_name}")
         f.close()
         
-    def build_obs_files(self, rels, rel_indices):      
+    def build_obs_files(self):      
         for relindex,name in rel_indices.items():       
-            f = open(f"{name}_obs.txt", "a")
+            f = open(f"data/{name}_obs.txt", "a")
             for h,r,t in rels:
                 if r != relindex:
                     continue
