717206b5d125006bdbf3738f96061431070afdfd
EwanHil <110049541+EwanHil@users.noreply.github.com>
2022-08-31 13:37:53 +0100

    Delete code_latest directory

--- code_latest/classes/Dataset.py
+++
@@ -1,8 +0,0 @@
-from pykeen.datasets import Nations
-from pykeen.datasets import CoDExSmall
-#Small class so we only need to change database in one place
-def get_dataset():
-    return Nations()
-
-def get_dataset_name():
-    return "Nations"
