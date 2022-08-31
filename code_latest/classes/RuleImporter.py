e6ec0712e3cb4da8990c26297ed79f63b83a0f89
EwanHil <110049541+EwanHil@users.noreply.github.com>
2022-08-31 08:26:43 +0100

    Delete code_latest directory

--- code_latest/classes/RuleImporter.py
+++
@@ -1,9 +0,0 @@
-class RuleImporter():
-    def __init__(self, filename='rules/alpha-10.txt'):
-        self.filename = filename
-        self.rules =[]
-        
-    def import_rules(self):
-        with open(self.filename, encoding='utf-8') as rules:
-            for rule in rules:
-                self.rules.append(rule)  
